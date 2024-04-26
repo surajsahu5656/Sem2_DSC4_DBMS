acfb723e1110c202daaf593fb2e4eec886ab9099
Suraj Sahu <122232215+surajsahu5656@users.noreply.github.com>
2024-04-26 16:30:09 +0530

    Add files via upload

---
+++ SOCIETY TABLE.sql
@@ -0,0 +1,18 @@
+USE RAMANUJAN_COLLEGE;
+
+INSERT INTO SOCIETY (SOCID, SOCNAME, MENTORNAME, TOTALSEATS) VALUES
+('SC001', 'Debating', 'Mr. Smith', 50),
+('SC002', 'Dance', 'Ms. Johnson', 40),
+('SC003', 'Sashakt', 'Mrs. Gupta', 30);
+
+
+INSERT INTO SOCIETY (SOCID, SOCNAME, MENTORNAME, TOTALSEATS) VALUES
+('SC004', 'Art Club', 'Ms. Thompson', 35),
+('SC005', 'Photography Society', 'Mr. Wilson', 20),
+('SC006', 'Coding Club', 'Mr. Brown', 50);
+
+
+INSERT INTO SOCIETY (SOCID, SOCNAME, MENTORNAME, TOTALSEATS) VALUES
+('SC007', 'Drama Club', 'Ms. Garcia', 25),
+('SC008', 'Environmental Club', 'Mr. Clark', 30),
+('SC009', 'Robotics Society', 'Mr. Miller', 40);
---
+++ STUDENT TABLE.sql
@@ -0,0 +1,21 @@
+USE RAMANUJAN_COLLEGE;
+
+INSERT INTO STUDENT (ROLL_NO, STUDENTNAME, COURSE, DOB) VALUES
+('S001', 'Alice', 'Computer Science', '2000-05-10'),
+('S002', 'Bob', 'Chemistry', '2001-03-15'),
+('S003', 'Charlie', 'Physics', '2000-09-20'),
+('S004', 'David', 'Mathematics', '1999-11-25');
+
+
+INSERT INTO STUDENT (ROLL_NO, STUDENTNAME, COURSE, DOB) VALUES
+('S005', 'Michael Johnson', 'Psychology', '2002-03-25'),
+('S006', 'Emily White', 'Biology', '2000-08-12'),
+('S007', 'David Lee', 'History', '2001-11-30');
+
+
+INSERT INTO STUDENT (ROLL_NO, STUDENTNAME, COURSE, DOB) VALUES
+('S008', 'Sophia Martinez', 'Economics', '2002-09-05'),
+('S009', 'Daniel Anderson', 'Pol Science', '2000-12-18'),
+('S010', 'Olivia Thomas', 'Eng Lit', '2001-04-30');
+
+
